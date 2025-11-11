1f17550554e403bc16e604eaf53c958401b2c7df
bryant-con-t <bry.y.valen@gmail.com>
2025-11-10 21:15:55 -0500

    Tema 01: ASP .NET Core MVC

---
+++ DSWI-cibertec-demo/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using DSWI_cibertec_demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSWI_cibertec_demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UsuarioModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Si la validación falla vuelve a mostrar el formulario con los errores
+                return View(model);
+            }
+
+            // Simulamos registro
+            TempData["Message"] = $"Usuario {model.Nombre} registrado correctamente.";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DSWI-cibertec-demo/Models/UsuarioModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSWI_cibertec_demo.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es olbigatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "La contraseña debe tener entre 4 y 100 caracteres")]
+        public string Clave { get; set; }
+
+        [EmailAddress(ErrorMessage = "Correo electrónico inválido")]
+        public string Correo { get; set; }
+
+        [Display(Name = "Fecha de registro")]
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
